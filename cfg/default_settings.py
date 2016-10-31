bcae8a46be1f7beb18b937450ef012077b310d73
Shahab <shahabalami@hotmail.com>
2016-10-30 20:49:06 -0400

    add config file

---
+++ cfg/default_settings.py
@@ -0,0 +1,11 @@
+'''
+Created on Oct 29, 2016
+
+@author: salami
+'''
+#app
+DEBUG= True
+SECRET_KEY= 'gfgpifghm+z3rpgq7$m-)orq4=#id!j-t_9w4qdrox2rrl7s2t'
+
+#db
+SQLALCHEMY_DATABASE_URI= 'postgresql://postgres:@localhost/chloroform'
